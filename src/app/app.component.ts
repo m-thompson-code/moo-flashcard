6438931c2e148be1070fa21cce57437949057309
Mark Thompson <mark.thompson@smpl.company>
2020-11-17 15:39:22 -0800

    added all the material components I wanted to use

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,61 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
+
+import { MatCheckboxChange } from '@angular/material/checkbox';
+import { MatAccordion } from '@angular/material/expansion';
+import { MatSnackBar } from '@angular/material/snack-bar';
+
+import firebase from 'firebase/app';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'moo-flashcard';
+export class AppComponent implements OnInit {
+    // @ViewChild(MatAccordion) accordion!: MatAccordion;
+
+    public checked: boolean = true;
+
+    public myForm!: FormGroup;
+    public newTopicForm!: FormGroup;
+
+    constructor(private fb: FormBuilder, private _snackBar: MatSnackBar) {
+
+    }
+
+    public ngOnInit(): void {
+        console.log(firebase);
+
+        this.myForm = this.fb.group({
+            notes: new FormControl({
+                value: 'default value',
+                disabled: false,
+            }),
+        });
+
+        this.newTopicForm = this.fb.group({
+            topicText: new FormControl({
+                value: '',
+                disabled: false,
+            }),
+        });
+
+        this.openSnackBar();
+
+        setTimeout(() => {
+            this.openSnackBar();
+        }, 1000 * 10);
+    }
+
+    public updateChecked(event: MatCheckboxChange): void {
+        console.log(event);
+        this.checked = event.checked;
+    }
+
+    public openSnackBar() {
+        this._snackBar.open('moo cow', undefined, {
+            duration: 5000,
+        });
+    }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,68 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatRippleModule } from '@angular/material/core';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatTooltipModule } from '@angular/material/tooltip';
+
+import firebase from 'firebase/app';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCyJMWOu6SInEhJfuJNfBP3CGYqLSDY52g",
+    authDomain: "moo-flashcard.firebaseapp.com",
+    databaseURL: "https://moo-flashcard.firebaseio.com",
+    projectId: "moo-flashcard",
+    storageBucket: "moo-flashcard.appspot.com",
+    messagingSenderId: "617891036670",
+    appId: "1:617891036670:web:dcf2f3a4493a4ecb5ad4af",
+    measurementId: "G-3N4JGNK7T7"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+import "firebase/auth";
+import "firebase/firestore";
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+       AppComponent
+    ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        BrowserAnimationsModule,
+        ReactiveFormsModule,
+
+        MatButtonModule,
+        MatCardModule,
+        MatCheckboxModule,
+        MatDividerModule,
+        MatExpansionModule,
+        MatFormFieldModule,
+        MatIconModule,
+        MatInputModule,
+        MatProgressBarModule,
+        MatProgressSpinnerModule,
+        MatRippleModule,
+        MatSnackBarModule,
+        MatToolbarModule,
+        MatTooltipModule,
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
