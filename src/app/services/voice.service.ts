d60f8dc4033aa8561a85c7b8823e864a11ada859
Mark Thompson <mark.thompson@smpl.company>
2020-11-18 04:00:11 -0800

    adjusted voices and rate for voice

--- src/app/services/voice.service.ts
+++ src/app/services/voice.service.ts
@@ -10,18 +10,6 @@ export class VoiceService {
     }
 
     public speak(text: string): void {
-        // Voices I liked
-        const voiceIndexes = [1, 2, 3, 4, 5];
-
-        const voiceSeed = voiceIndexes[Math.floor(Math.random() * voiceIndexes.length)];
-
-        this.silence();
-
-        const synth =  window.speechSynthesis;
-
-        const msg = new SpeechSynthesisUtterance();
-        const voices = synth.getVoices();
-
         // msg.voice = voices[1]; // Note: some voices don't support altering params
         // // (msg as any).voiceURI = 'native';
         // // msg.volume = 1; // 0 to 1
@@ -34,16 +22,32 @@ export class VoiceService {
         // //     console.log('Finished in ' + event.elapsedTime + ' seconds.');
         // // };
 
+        // Voices I liked
+        const voiceIndexes = [0, 1, 4, 5];
+
+        const voiceSeed = voiceIndexes[Math.floor(Math.random() * voiceIndexes.length)];
+
+        this.silence();
+
+        const msg = new SpeechSynthesisUtterance();
+        const voices = window.speechSynthesis.getVoices();
+
+        (msg as any).voiceURI = 'native';
+
+        if (voiceSeed === 4) {
+            msg.rate = .8;
+        } else if (voiceSeed === 1) {
+            msg.rate = .9;
+        }
+
         msg.voice = voices[voiceSeed];
         msg.text = text;
         msg.lang = 'en-US';
 
-        synth.speak(msg);
+        window.speechSynthesis.speak(msg);
     }
 
     public silence() {
-        const synth =  window.speechSynthesis;
-
-        synth.cancel(); // utterance1 stops being spoken immediately, and both are removed from the queue
+        window.speechSynthesis.cancel(); // utterance1 stops being spoken immediately, and both are removed from the queue
     }
 }
